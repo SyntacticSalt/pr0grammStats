4b1c8501c785595e8ab50b02bda4f57b08802b0a
knusprigeswuerstchen <knusprigeswuerstchen@users.noreply.github.com>
2014-12-22 18:17:00 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -2,13 +2,9 @@ function  generateStats(data){
 var itemCount = data.items.length;
 var upTotal;
 var downTotal;
-var promotedTotal;
 for(i=0;i >= itemCount;i++){
-upTotal += data.items[i].up;
-downTotal += data.items[i].down;
-if(data.items[i].promoted != 0){
-promotedTotal++;
-}
+upTotal += data.item[i].up;
+downTotal += data.item[i].down;
 }
 var controversy = 0;
 if(upTotal >= downTotal){
@@ -17,11 +13,14 @@ controversy = downTotal/upTotal;
 controversy = 1/(downTotal/upTotal);
 }
 controversy = Math.round(100*controversy);
-var promotedpercent = Math.round(100*(promotedTotal/itemCount));
+var promotedpercent = Math.round(100*(promoted/itemCount));
 var avgVote = 0;
 var benisTotal = 0;
 benisTotal = upTotal-downTotal;
+
+return "upTotal" + upTotal + "downTotal" + downTotal + "itemCount" + itemCount;
+/*
 avgVote = Math.round(benisTotal / itemCount);
 var markupOut ="<span id='avg'>&empty;"+ avgVote + "</span><span>Benis</span><br/><span style='color: green' id='up'>+" + upTotal + "</span><span id='seperator'> &#124;</span><span style='color:red' id='down'>-"+ downTotal + " </span><br/><span id='controversy'>" + controversy + "% kontrovers</span><br/><span id='promoted'>"+promotedpercent+" % in /top</span>";
-return markupOut;
+return markupOut;*/
 }
