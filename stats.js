8f692d1cf7446b6b99c48cf36cc8de2ad445de4e
knusprigeswuerstchen <knusprigeswuerstchen@users.noreply.github.com>
2014-12-22 18:47:50 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -21,11 +21,8 @@ var promotedpercent = Math.round(100*(window.promoted/itemCount));
 var avgVote = 0;
 var benisTotal = 0;
 benisTotal = upTotal-downTotal;
-
-return "upTotal" + upTotal + "downTotal" + downTotal + "itemCount" + itemCount;
-/*
 avgVote = Math.round(benisTotal / itemCount);
 var markupOut ="<span id='avg'>&empty;"+ avgVote + "</span><span>Benis</span><br/><span style='color: green' id='up'>+" + upTotal + "</span><span id='seperator'> &#124;</span><span style='color:red' id='down'>-"+ downTotal + " </span><br/><span id='controversy'>" + controversy + "% kontrovers</span><br/><span id='promoted'>"+promotedpercent+" % in /top</span>";
-return markupOut;*/
+return markupOut;
 }
 
